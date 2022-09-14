139aa13c1d3d520239f76757a6edc7830f5b2bb8
CloneTwo7 <travisp0027@gmail.com>
2022-09-14 06:25:18 +0000

    next submission

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int lineNum(char*, char*, int);
 
 int main(int argc, char** argv) {
 
-	char *word = "mellow";
+	char word[9] = "fi sh";
 	printf("%s is found on line %d\n", word, lineNum("tiny_9", word, 9));
 
 	return(0);
